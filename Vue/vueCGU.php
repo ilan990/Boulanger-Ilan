f8d854300823b6b3d6188b91a76e0ae7f2b7565c
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-01-08 22:35:54 +0100

    Ajout des conditions générales

--- Vue/vueCGU.php
+++ Vue/vueCGU.php
@@ -1,1 +1,39 @@
 <?php $this->titre = "Boulanger'Ilan - CGU"; ?>
+
+<div class="container" >
+    <h1>Conditions générales d'utilisation</h1>
+
+    <h2>Article 1 : Objet</h2>
+    <p>
+        Les présentes conditions générales d'utilisation (CGU) s'appliquent à toutes les commandes passées sur le site Internet de la boulangerie "Boulanger'Ilan". Elles ont pour objet de définir les conditions de vente entre la boulangerie et l'acheteur, de la commande jusqu'à la livraison des produits.
+    </p>
+    <h2>Article 2 : Accès au site</h2>
+    <p>
+        L'accès au site de la boulangerie "Boulanger'Ilan" est ouvert à tous. Toutefois, la boulangerie se réserve le droit de limiter ou d'interdire l'accès au site à tout utilisateur ne respectant pas les présentes CGU.
+    </p>
+    <h2>Article 3 : Utilisation du site</h2>
+    <p>
+        L'utilisateur s'engage à utiliser le site de manière loyale et légale, et à ne pas perturber son fonctionnement. Il s'interdit notamment de :
+
+        Télécharger ou envoyer du contenu illégal, menaçant, diffamatoire, obscène ou offensant ;
+        Usurper l'identité d'un tiers ou diffuser des informations fausses ou trompeuses ;
+        Tenter de pénétrer dans les systèmes informatiques de la boulangerie ou de ses partenaires ;
+        Collecter ou utiliser les données personnelles d'autres utilisateurs sans leur consentement.
+    </p>
+    <h2>Article 4 : Propriété intellectuelle</h2>
+    <p>
+        Le contenu du site de la boulangerie "Boulanger'Ilan", y compris les textes, images, sons et vidéos, est protégé par le droit de la propriété intellectuelle. Toute reproduction, utilisation, modification, diffusion ou commercialisation de ce contenu sans l'autorisation expresse de la boulangerie est interdite.
+    </p>
+    <h2>Article 5 : Responsabilité</h2>
+    <p>
+        La boulangerie "Boulanger'Ilan" s'engage à mettre en œuvre tous les moyens nécessaires pour garantir la disponibilité et la sécurité du site. Toutefois, elle ne pourra être tenue responsable des éventuelles interruptions de service, des erreurs, ou des dommages causés aux utilisateurs.
+    </p>
+    <h2>Article 6 : Modification des CGU</h2>
+    <p>
+        La boulangerie se réserve le droit de modifier à tout moment les présentes CGU. Les utilisateurs sont invités à les consulter régulièrement pour prendre connaissance de toute éventuelle modification.
+    </p>
+    <h2>Article 7 : Loi applicable et juridiction compétente</h2>
+    <p>
+        Les présentes CGU sont régies par la loi française. Tout litige relatif à leur interprétation ou à leur exécution sera de la compétence des tribunaux français.
+    </p>
+</div>
--- Vue/vueCGV.php
+++ Vue/vueCGV.php
@@ -1,1 +1,37 @@
 <?php $this->titre = "Boulanger'Ilan - CGV"; ?>
+
+<div class="container">
+    <h1>Conditions générales de vente</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porttitor, nisl a imperdiet sollicitudin, nisi ipsum malesuada nunc, non viverra metus felis non tellus. Aliquam erat volutpat. Suspendisse potenti. In ultricies vehicula orci, vel ultricies ipsum facilisis vel.
+    </p>
+
+    <h2>Article 1 : Objet</h2>
+    <p>
+        Les présentes conditions générales de vente (CGV) s'appliquent à toutes les commandes passées sur le site Internet de la boulangerie "Boulanger'Ilan". Elles ont pour objet de définir les conditions de vente entre la boulangerie et l'acheteur, de la commande jusqu'à la livraison des produits.
+    </p>
+    <h2>Article 2 : Prix</h2>
+    <p>
+        Les prix indiqués sur le site Internet de la boulangerie "Boulanger'Ilan" sont des prix en euros toutes taxes comprises (TTC). Ils sont fermes et non révisables pendant leur période de validité. La boulangerie se réserve le droit de modifier ses prix à tout moment, mais s'engage à appliquer les tarifs en vigueur qui auront été indiqués au moment de la commande.
+    </p>
+    <h2>Article 3 : Paiement</h2>
+    <p>
+        Le paiement des commandes peut être effectué par carte bancaire ou par virement bancaire. Le paiement par carte bancaire est sécurisé grâce au système de paiement en ligne de notre partenaire bancaire.
+    </p>
+    <h2>Article 4 : Livraison</h2>
+    <p>
+        La livraison des produits commandés est effectuée à l'adresse de livraison indiquée par l'acheteur. La boulangerie s'engage à livrer les produits dans les délais indiqués sur le site Internet, sous réserve de disponibilité des produits. En cas de retard de livraison, l'acheteur sera informé par e-mail ou par téléphone.
+    </p>
+    <h2>Article 5 : Retour et remboursement</h2>
+    <p>
+        En cas de non-conformité des produits livrés, l'acheteur dispose d'un délai de 7 jours ouvrables pour en informer la boulangerie et obtenir un échange ou un remboursement. Les frais de retour seront alors pris en charge par la boulangerie
+    </p>
+    <h2>Article 6 : Responsabilité</h2>
+    <p>
+        La boulangerie "Boulanger'Ilan" ne peut être tenue responsable des dommages résultant de l'utilisation des produits achetés. La responsabilité de la boulangerie ne pourra être engagée en cas de force majeure ou de faits imputables à l'acheteur.
+    </p>
+    <h2>Article 7 : Données personnelles</h2>
+    <p>
+        Les données personnelles de l'acheteur sont collectées et traitées par la boulangerie "Boulanger'Ilan" dans le cadre de la gestion de la commande et de la relation client. Elles sont conservées pendant la durée nécessaire à la gestion de la commande et sont destinées à la boulangerie et à ses partenaires. Conformément
+    </p>
+</div>
